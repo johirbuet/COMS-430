4292bb7d2e3deb40ab1a086f68613967ff466a89
johirbuet <johirbuet@gmail.com>
2014-12-27 18:42:07 -0600

    Md Johirul Islam

---
+++ COMS430/src/com/johir/LazyInitRace.java
@@ -0,0 +1,53 @@
+package com.johir;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class LazyInitRace implements Servlet {
+	private ExpensiveObject instance=null;
+	public ExpensiveObject getInstance(){
+		if(instance==null)
+			instance=new ExpensiveObject();
+		return instance;
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+class ExpensiveObject{
+	
+}
