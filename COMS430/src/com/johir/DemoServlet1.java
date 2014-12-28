fd1943082f09ed74571a0dce91157941ca11371a
johirbuet <johirbuet@gmail.com>
2014-12-27 20:19:13 -0600

    Md Johirul Islam

---
+++ COMS430/src/com/johir/DemoServlet1.java
@@ -0,0 +1,50 @@
+package com.johir;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class DemoServlet1 implements Servlet {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<html><body>");
+        out.println("WELCOME SERVLET");
+        out.println("</body></html>");
+		
+	}
+
+}
