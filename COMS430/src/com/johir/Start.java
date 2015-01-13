7a89755a2fdb200d36254b5f50a7c7087a1601f3
johirbuet <johirbuet@Johir>
2015-01-13 16:36:40 -0600

    Welcome

---
+++ COMS430/src/com/johir/Start.java
@@ -0,0 +1,13 @@
+package com.johir;
+
+import javax.swing.JOptionPane;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Welcome to COMS 430");
+
+	}
+
+}
